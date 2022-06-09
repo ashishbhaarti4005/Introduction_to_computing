b4d994819b8750722ad9fc78f513a52fc05cbaad
Ashish bharti <98837841+ashishbhaarti4005@users.noreply.github.com>
2022-06-09 22:18:38 +0530

    Add files via upload

---
+++ assigment 3/output aasgn 3.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: C:/Users/ashis/OneDrive/Desktop/assigment 3/input assgn 3.py ====
+Enter a number to convert it into its binary equivalent:986
+ Its Binary value:
+01111011010Select an operation to perform : 
+1. ADD
+2. SUBTRACT
+3. MULTIPLY
+4. DIVIDE
+1
+Enter first number: 65
+Enter second number: 23
+The sum is :88
+Enter the value of a :12
+Enter the value of b :45
+Enter the value of c :11
+Ans = 627
+Enter the value of n :5
+Ans = 10.0
+Ans = 4*math.pi*r**2
+Ans = math.sqrt(r*(math.cos(a))**2+r*(math.sin(a))**2
+Ans = y2-y1)/(x2-x1
+0 1 2 3 4 
+3 4 5 6 7 8 9 
+4 7 10 
+15 13 11 9 7 
+7
+This program finds the total molecular weight of a combination of hydrogen, carbon and oxygen atoms :
+How many hydrogen atoms? 4
+Carbon? 1
+Oxygen? 1
+The total molecular weight is 32.04186
